1f1861eeb7132be67eb2d9fb0cb363b39af21028
田森林 <2692685085@qq.com>
2019-04-18 20:15:21 +0800

    four

--- src/main/java/com/bwei/demo/api/BitcoinApi.java
+++ src/main/java/com/bwei/demo/api/BitcoinApi.java
@@ -1,7 +1,10 @@
 package com.bwei.demo.api;
 
+import com.alibaba.fastjson.JSONObject;
+
 /**
  * Created by 田森林 on 2019/4/18.
  */
 public interface BitcoinApi {
+    JSONObject getBlock(String temphash);
 }
--- src/main/java/com/bwei/demo/service/MiscService.java
+++ src/main/java/com/bwei/demo/service/MiscService.java
@@ -1,7 +1,11 @@
 package com.bwei.demo.service;
 
+import org.springframework.scheduling.annotation.Async;
+
 /**
  * Created by 田森林 on 2019/4/18.
  */
 public interface MiscService {
+    @Async
+    void importFromHash(String blockHash, Boolean isClean);
 }
