7e28f648066f5222f11670ece36ac5fc426e3592
田森林 <2692685085@qq.com>
2019-04-18 20:20:22 +0800

    four

--- src/main/java/com/bwei/demo/api/BitcoinApi.java
+++ src/main/java/com/bwei/demo/api/BitcoinApi.java
@@ -1,10 +1,16 @@
 package com.bwei.demo.api;
 
 import com.alibaba.fastjson.JSONObject;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 
 /**
  * Created by 田森林 on 2019/4/18.
  */
+@FeignClient(name = "bitcin",url ="http://localhost:18332")
 public interface BitcoinApi {
+
+
+    @GetMapping("/rest/block/{blockhash}.json")
     JSONObject getBlock(String temphash);
 }
